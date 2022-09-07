161d0f68d0319b36ee2b6b562cc48b3a63c3d145
Deangellis Berg <deangellisbergjob@gmail.com>
2022-09-07 01:20:38 -0300

    other componets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Ola mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
-    </>
-  )
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-contanier">
+          <SalesCard/>
+          </div>
+          </section>
+          </main>
+
+
+          </>
+          )
 
 }
 
 
-export default App
+          export default App
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-contenier">
+                <img src={logo} alt="DSMeta"/>
+                    <h1>DSMeta</h1>
+                    <p>
+                        Desenvolvido por
+
+                        <a   href="www.linkedin.com/in/deangellis-berg-bezerra-da-silva-4890b2139">www.linkedin.com/in/deangellis-berg-bezerra-da-silva-4890b2139</a>
+                    </p>
+
+            </div>
+
+
+        </header>
+
+    )
+
+}
+
+
+export default Header 
